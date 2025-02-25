df120e6db83223e7aaa251ae12906c40c7fbc576
turkoglusinan <sinanturkoglu99@gmail.com>
2025-02-25 11:38:10 -0500

    update information for pulumi

--- Pulumi/index.js
+++ Pulumi/index.js
@@ -6,6 +6,7 @@ const instanceNumber = 3;
 const instances = [];
 
 // Create an AWS resource (EC2)
+// need to update your ami ID and key pair
 for (let i = 0; i < instanceNumber; i++){
     const instance = new aws.ec2.Instance(`new-instance-${i}`,{
         ami: "ami-04b4f1a9cf54c11d0",
