6f13f7fa1ad7596c8c50dd1539d1b9031fd79783
turkoglusinan <sinanturkoglu99@gmail.com>
2025-02-25 11:09:34 -0500

    pulumi create EC2 instances with for loop

---
+++ Pulumi/index.js
@@ -0,0 +1,24 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+const instanceNumber = 3;
+
+const instances = [];
+
+// Create an AWS resource (EC2)
+for (let i = 0; i < instanceNumber; i++){
+    const instance = new aws.ec2.Instance(`new-instance-${i}`,{
+        ami: "ami-04b4f1a9cf54c11d0",
+        instanceType: "t2.micro",
+        keyName: "sinan-ec2-keypair",
+        tags: {
+            Name: `new-ec2-instance-${i}`
+        }
+    }
+    );
+    instances.push(instance)
+}
+
+// Export the EC2 instances IDs
+export const instancesIds = instances.map(instance => instance.id);
+
